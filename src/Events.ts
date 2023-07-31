7972e3ab0f2e41a7953bfc994eed5ee88623a780
CENSOR_1337 <viixxivvimm@gmail.com>
2023-07-31 11:38:02 +0700

    rename:

--- src/Events.ts
+++ src/Events.ts
@@ -5,12 +5,12 @@ import { Citizen } from "@cfx/shared";
 import cfx from "@cfx/shared";
 
 export class Events extends SharedEvents {
-	public static onServer(eventName: string, handler: listenerType): SharedEvent {
-		return new SharedEvent(eventName, handler, true, false);
+	public static onServer(eventName: string, listener: listenerType): SharedEvent {
+		return new SharedEvent(eventName, listener, true, false);
 	}
 
-	public static onceServer(eventName: string, handler: listenerType): SharedEvent {
-		return new SharedEvent(eventName, handler, true, true);
+	public static onceServer(eventName: string, listener: listenerType): SharedEvent {
+		return new SharedEvent(eventName, listener, true, true);
 	}
 
 	public static emitServer = Citizen.triggerServerEvent;
