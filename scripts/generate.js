55d72c5f6598e0ba2188f4edeb85286b5cfb462a
Metee <viixxivvimm@gmail.com>
2023-08-20 14:08:36 +0700

    refactor: using nativeName

--- scripts/generate.js
+++ scripts/generate.js
@@ -16,7 +16,7 @@ const debugNatives = [
 ]
 const bDebug = false;
 const header = `
-import { Vector3 } from "@cfx/shared";
+import { Vector3 } from "cfx-shared";
 //@ts-ignore
 const Citizen = global.Citizen;
 //@ts-ignore
@@ -113,7 +113,7 @@ function generateNatives() {
 
     // Sort by name
     allNatives.sort((a, b) => {
-        if (a.altName > b.altName) {
+        if (a.nativeName > b.nativeName) {
             return 1;
         } else {
             return -1;
